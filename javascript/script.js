3537a7db8917100ee578f1093fbb487e0e073088
Janani72 <janani11601@gmail.com>
2025-06-14 16:06:07 +0530

    commited js

---
+++ javascript/script.js
@@ -0,0 +1,76 @@
+// var a="10";
+// var b=10;
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+
+// var num=2;
+// var result=num%2==0?"even":"odd";
+// console.log(result);
+
+// var mark=34;
+// var result=(mark>=80)? "0":(mark>=60)? "A+":(mark>=35)? "p": "f";
+// console.log(result);
+
+
+// if (mark>=80){
+//     console.log("0");
+// }
+// else if( mark>=60){
+//     console.log("A+");
+// }
+// else{
+//     console.log("f");
+// }
+
+
+ 
+
+
+
+// console.log(typeof a);
+// console.log(typeof b);
+// a=true;
+// console.log(typeof a);
+// var c;
+// console.log(typeof c);
+// var c=null;
+// console.log(typeof c);
+
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(10,20)
+
+var sum = ()=>{
+    var a=10;
+    var b=20;
+    console.log(a+b);
+}
+sum();
+
+
+
+let arr=[10,20,30,40];
+let arr2=[...arr,50,60,70]
+console.log(arr);
+console.log(arr2);
+
+
+var marks=[90,95,96,100,99];
+var [m1,m2,m3,m4,m5] =marks;
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(m4);
+console.log(m5);
+
+
+
+
+
+
+
